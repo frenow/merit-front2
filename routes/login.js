1ecbb5cf7428f5478d1e8f2aab54c19acfe8b23d
frenow <frenow@gmail.com>
2019-11-11 14:09:31 -0300

    add firebase database

--- routes/login.js
+++ routes/login.js
@@ -1,37 +1,7 @@
 const express = require('express');
 const router = express.Router();
-var axios = require('axios');
 
-router.post('/', (req, res) => {
-  const token = req.body.token;  
-  console.log('valor token: '+token);
-
-    setTimeout(function() {    
-        axios(
-            {
-                url: 'https://fcm.googleapis.com/fcm/send',
-                method: 'post',
-                headers: {
-                  "Content-Type":"application/json",
-                  "Authorization":"key=AAAAjgI0vfM:APA91bGo66e6F7ikV8k4SwfsjSOqIoTZJ7vFek--csDOD6oCXt0chO99UP9sI0v30AYd6HlU_NhipN6UFuu1Wowj1gcAGTgxqLaDatPFC-bvRERnRnW_w0Ed9FQnvKgGDNtCDWJOmJ3e"
-                },
-                data: {
-                    "notification": {
-                      "title":"Voce acabe de receber 10und Merit",
-                      "text":"O usuario 123 enviou 10und Merit para voce.",
-                      "sound":"default"
-                    },
-                    "to":token,
-                    "priority":"high"
-                  }
-              }
-        ).then((r) => {
-            console.log(r.data)
-          }).catch((error) => {
-            console.log(error)
-          });
-        }, 5000);
-    
+router.get('/', (req, res) => {
     res.render('login');
 });
 
