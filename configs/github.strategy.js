779a74ea8ae31208fdb2532dd642d0fe76cce6e5
frenow <frenow@gmail.com>
2019-11-12 13:01:09 -0300

    fix callback passport heroku

--- configs/github.strategy.js
+++ configs/github.strategy.js
@@ -5,7 +5,7 @@ passport.use(new GitHubStrategy({
     clientID: 'Iv1.51fb7de2f224f187',
     clientSecret: 'afc8c17d493cf774d04970b4656a5e7a93d9d040',
     //callbackURL: "http://localhost:3000/auth/github/callback"
-    callbackURL: "https://merit-front.herokuapp.com/auth/facebook/callback",
+    callbackURL: "https://merit-front.herokuapp.com/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, cb) {
     return cb(null, profile);
